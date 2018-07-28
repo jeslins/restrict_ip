c240f3efb66b6ed3e957f0bc1d57803e95794fb0
Jay Friendly <friendly@jaypan.com>
2018-07-28 11:46:18 +1000

    Altering code to Drupal coder standard

--- js/restrict_ip.js
+++ js/restrict_ip.js
@@ -1,15 +1,21 @@
-/*globals jQuery, Drupal, location*/
-/*jslint plusplus: true, sloppy: true, white: true */
+/**
+ * @file
+ * Adds JS functionality to the access denied page of the Restrict IP module.
+ */
 
-(function($)
-{
-	// We do not use Drupal.behaviors, as the Drupal object
-	// is not available due to drupal.js having been removed
-	$(document).ready(function()
-	{
-		var mailDiv, contactMail;
-		mailDiv = $("#restrict_ip_contact_mail");
-		contactMail = mailDiv.text().replace("[at]", "@");
-		mailDiv.html("<a href=\"mailto:" + contactMail + "\">" + contactMail + "</a>");
-	});
-}(jQuery));
+/*globals jQuery, document*/
+/*jslint white: true */
+
+(function ($, document) {
+
+  "use strict";
+
+  // We do not use Drupal.behaviors, as the Drupal object is not available due
+  // to drupal.js having been removed.
+  $(document).ready(function () {
+    var mailDiv, contactMail;
+    mailDiv = $("#restrict_ip_contact_mail");
+    contactMail = mailDiv.text().replace("[at]", "@");
+    mailDiv.html("<a href=\"mailto:" + contactMail + "\">" + contactMail + "</a>");
+  });
+}(jQuery, document));
--- restrict_ip.api.php
+++ restrict_ip.api.php
@@ -6,45 +6,41 @@
  */
 
 /**
- * Add regions to be whitelisted even when the user has been denied access
+ * Add regions to be whitelisted even when the user has been denied access.
  *
- * @return
+ * @return array
  *   An array of keys representing regions to be allowed even when
  *   the user is denied access by IP. These keys can be found by
  *   in the .info file for the theme, as region[KEY] = Region Name
  *   where KEY is the key to be returned in the return array
  *   of this function.
  */
-function hook_restrict_ip_whitelisted_regions()
-{
-	return array('sidebar_first');
+function hook_restrict_ip_whitelisted_regions() {
+  return array('sidebar_first');
 }
 
 /**
- * Add js keys to be whitelisted even when the user has been denied access
+ * Add js keys to be whitelisted even when the user has been denied access.
  *
- * @return
+ * @return array
  *   An array of keys representing javascripts to be allowed even when
  *   the user is denied access by IP. These keys can be found by
  *   as the keys in hook_js_alter().
  */
-function hook_restrict_ip_whitelisted_js_keys()
-{
-	return array('misc/jquery.once.js');
+function hook_restrict_ip_whitelisted_js_keys() {
+  return array('misc/jquery.once.js');
 }
 
 /**
  * Alter the Restrict IP Access Denied page.
  *
- * @param $page
+ * @param array $page
  *   The render array for the access deneid page passed by reference.
  */
-function hook_restrict_ip_access_denied_page_alter(&$page)
-{
-	$page['additional_information'] = array
-	(
-		'#markup' => t('Additional information to be shown on the Restrict IP Access Denied page'),
-		'#prefix' => '<p>',
-		'#suffix' => '</p>',
-	);
+function hook_restrict_ip_access_denied_page_alter(array &$page) {
+  $page['additional_information'] = array(
+    '#markup' => t('Additional information to be shown on the Restrict IP Access Denied page'),
+    '#prefix' => '<p>',
+    '#suffix' => '</p>',
+  );
 }
